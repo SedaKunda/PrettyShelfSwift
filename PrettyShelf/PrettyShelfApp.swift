ded69c2e0ba6fdf7f70c31dfd025ed458e5f902b
Seda Kunda <seda.kunda@zuhlke.com>
2022-01-18 12:27:10 +0000

    Initial Commit

---
+++ PrettyShelf/PrettyShelfApp.swift
@@ -0,0 +1,17 @@
+//
+//  PrettyShelfApp.swift
+//  PrettyShelf
+//
+//  Created by Seda Kunda on 18/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct PrettyShelfApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
