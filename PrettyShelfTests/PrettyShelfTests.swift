8a54196773faf40abfe317447c916dcfb09d9c11
Seda Kunda <seda.kunda@zuhlke.com>
2022-02-08 12:42:54 +0000

    unit tests initialisation

--- PrettyShelfTests/PrettyShelfTests.swift
+++ PrettyShelfTests/PrettyShelfTests.swift
@@ -9,7 +9,7 @@ import XCTest
 @testable import PrettyShelf
 
 class PrettyShelfTests: XCTestCase {
-
+//test isbn 9780896937338 or 9780141378664
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
